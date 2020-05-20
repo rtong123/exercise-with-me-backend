95e449d6bf732447fb9ad01d269aa8b947a0a3ae
Rebecca Tong <rtong@buffalo.edu>
2020-05-20 16:43:54 -0400

    adding index method

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -1,2 +1,26 @@
 class RoutinesController < ApplicationController
+
+  def create
+  end
+
+  def index
+    @routines = Routine.all
+    render json: @routines
+    # fetch request for all routines
+  end
+
+
+  def show
+  end
+
+  def destroy
+  end
+
+  private
+
+  def  routine_params
+    params.require(:routine).permit(:title,:start_date,:days,:body_weight)
+  end
+  # permit these attributes
+
 end
