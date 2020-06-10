16b4e88ad370243d02491a4ce21ebe9bf63ad705
Rebecca Tong <rtong@buffalo.edu>
2020-06-10 15:54:08 -0400

    changed to nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :exercises
-  resources :routines
+  resources :routines do
+    resources :exercises
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
